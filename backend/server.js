5b46a5ee5c1f519a1ce281bd92528efa36c095f7
umesh <umeshsavaliya777@gmail.com>
2026-02-05 16:12:46 +0530

    Created working chat-app

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,48 @@
-const grpc = require('@grpc/grpc-js')
-const protoLoader = require('@grpc/proto-loader')
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const path = require("path");
 
-const PROTO_PATH = './proto/hello.proto'
+const PROTO_PATH = path.join(__dirname, "proto/chat.proto");
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, {});
+const chatProto = grpc.loadPackageDefinition(packageDefinition).chat;
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {})
-const helloProto = grpc.loadPackageDefinition(packageDefinition).hello
+const activeUsers = new Map();
 
-function sayHello(call, callback) {
-  const name = call.request.name
-  callback(null, { message: `Hello ${name}! 👋` })
+function sendMessage(call, callback) {
+  const { from, to, message } = call.request;
+  console.log(`${from} -> ${to}: ${message}`);
+
+  const targetStream = activeUsers.get(to);
+
+  if (targetStream) {
+    targetStream.write({
+      from: from,
+      to: to,
+      message: message,
+      timestamp: Date.now()
+    });
+  } else {
+    console.log(`[WARN] User ${to} is not online.`);
+  }
+  
+  callback(null, {});
 }
 
-function main() {
-  const server = new grpc.Server()
+function subscribe(call) {
+  const userId = call.request.userId;
 
-  server.addService(helloProto.HelloService.service, {
-    SayHello: sayHello
-  })
+  activeUsers.set(userId, call);
+  console.log(` ${userId} is now online.`);
 
-  server.bindAsync(
-    '0.0.0.0:50051',
-    grpc.ServerCredentials.createInsecure(),
-    () => {
-      console.log('gRPC server running on port 50051')
-    }
-  )
+  call.on("cancelled", () => {
+    activeUsers.delete(userId);
+    console.log(`${userId} went offline.`);
+  });
 }
 
-main()
+const server = new grpc.Server();
+server.addService(chatProto.ChatService.service, { sendMessage, subscribe });
+
+server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), () => {
+  console.log("Chat Server running on port 50051");
+});
